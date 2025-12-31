e80952b032be52bc1a5501bf642241818a9e7226
zaidkhan-div <zaidscestudent@gmail.com>
2025-12-31 10:55:36 +0500

    deplyed on vercel the backend

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
-
+export default app;
